a4389ea65f8dce788de6a18791db35b3c9e058d1
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-08-07 17:58:47 -0500

    Fix assert match in AssetNotPrecompiled tests

--- test/assets_test.rb
+++ test/assets_test.rb
@@ -228,7 +228,7 @@ module ApplicationTests
 
       get '/posts'
       assert_match(/AssetNotPrecompiledError/, last_response.body)
-      assert_match(/app.js isn't precompiled/, last_response.body)
+      assert_match(/app\.js isn.+t precompiled/, last_response.body)
     end
 
     test "assets raise AssetNotPrecompiledError when manifest file is present and requested file isn't precompiled if digest is disabled" do
@@ -252,7 +252,7 @@ module ApplicationTests
 
       get '/posts'
       assert_match(/AssetNotPrecompiledError/, last_response.body)
-      assert_match(/app.js isn't precompiled/, last_response.body)
+      assert_match(/app\.js isn.+t precompiled/, last_response.body)
     end
 
     test "precompile properly refers files referenced with asset_path and and run in the provided RAILS_ENV" do
