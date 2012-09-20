973b00df63a43c885e8041051e8887e2a5bdf0f9
Joshua Peek <josh@joshpeek.com>
2012-09-20 14:13:31 -0500

    Test basic helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,18 @@
 require 'test/unit'
 
+require 'action_view'
+
 class HelperTest < Test::Unit::TestCase
-  def test_truth
-    assert true
+  def setup
+    @view = ActionView::Base.new
+    @view.config.assets_dir = File.expand_path("..", __FILE__)
+  end
+
+  def test_javascript_path
+    assert_equal "/javascripts/foo.js", @view.javascript_path("foo")
+  end
+
+  def test_stylesheet_path
+    assert_equal "/stylesheets/foo.css", @view.stylesheet_path("foo")
   end
 end
