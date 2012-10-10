f75fa979f12b316fee052399cfa2748d122ae3c0
Joshua Peek <josh@joshpeek.com>
2012-10-10 17:12:20 -0500

    Don't create sprockets env if compile is disabled

--- lib/sprockets/rails.rb
+++ lib/sprockets/rails.rb
@@ -33,20 +33,22 @@ module Sprockets
             config.assets.manifest
           end
           define_method :assets_environment do
-            app.assets if config.assets.compile
+            app.assets
           end
         end
 
-        app.assets = Sprockets::Environment.new(app.root.to_s) do |env|
-          env.version = ::Rails.env + "-#{config.assets.version}"
+        if config.assets.compile
+          app.assets = Sprockets::Environment.new(app.root.to_s) do |env|
+            env.version = ::Rails.env + "-#{config.assets.version}"
 
-          if config.assets.cache_store != false
-            env.cache = ActiveSupport::Cache.lookup_store([:file_store, "#{config.root}/tmp/cache/assets/#{::Rails.env}"])
-          end
+            if config.assets.cache_store != false
+              env.cache = ActiveSupport::Cache.lookup_store([:file_store, "#{config.root}/tmp/cache/assets/#{::Rails.env}"])
+            end
 
-          env.context_class.class_eval do
-            include ::Sprockets::Rails::Helper
-            include config_helpers
+            env.context_class.class_eval do
+              include ::Sprockets::Rails::Helper
+              include config_helpers
+            end
           end
         end
 
@@ -60,18 +62,16 @@ module Sprockets
       end
 
       config.after_initialize do |app|
-        config = app.config
         return unless app.assets
 
+        config = app.config
         config.assets.paths.each { |path| app.assets.append_path(path) }
 
         app.assets.js_compressor  = config.assets.js_compressor
         app.assets.css_compressor = config.assets.css_compressor
 
-        if config.assets.compile
-          app.routes.prepend do
-            mount app.assets => config.assets.prefix
-          end
+        app.routes.prepend do
+          mount app.assets => config.assets.prefix
         end
 
         if config.assets.digest
