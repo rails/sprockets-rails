b11e44d6130dd533158f2928c65ec82bec980b3f
Brandon Beacher <brandon.beacher@gmail.com>
2013-01-13 12:38:43 -0500

    Ensure precompile setting from app is applied to rake task.
    * Rails calls `load_tasks` prior to the app's configure block.
    * This results in the sprockets rake task being initialized without the app's assets.precompile setting.
    * This change lazily evaluates the assets setting in the rake task so it will reflect the eventual configuration of assets.precompile.

--- lib/sprockets/railtie.rb
+++ lib/sprockets/railtie.rb
@@ -61,7 +61,7 @@ module Sprockets
       Sprockets::Rails::Task.new do |t|
         t.environment = lambda { app.assets }
         t.output      = File.join(app.root, 'public', app.config.assets.prefix)
-        t.assets      = app.config.assets.precompile
+        t.assets      = lambda { app.config.assets.precompile }
         t.cache_path  = "#{app.config.root}/tmp/cache/assets"
       end
     end
