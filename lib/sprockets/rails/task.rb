59922ecd19a81bf504be8e10ed624ae011a8360d
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-03-28 17:30:51 -0500

    Revert "Load gems of assets group before of precompile"
    This reverts commit bbc19aa2f54abb5dcddfc0a9bb0ec79b34bde128.
    The assets group doesn't exist anymore in Rails default Gemfile

--- lib/sprockets/rails/task.rb
+++ lib/sprockets/rails/task.rb
@@ -12,8 +12,6 @@ module Sprockets
           # Override this task change the loaded dependencies
           desc "Load asset compile environment"
           task :environment do
-            # Load gems in assets group of Gemfile
-            Bundler.require(:assets) if defined?(Bundler)
             # Load full Rails environment by default
             Rake::Task['environment'].invoke
           end
