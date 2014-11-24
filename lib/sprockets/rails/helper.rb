b519aae71a2dc98bfd93bed336610515d649a89f
Joshua Peek <josh@joshpeek.com>
2014-11-23 20:05:57 -0800

    Avoid private sprockets env api

--- lib/sprockets/rails/helper.rb
+++ lib/sprockets/rails/helper.rb
@@ -197,10 +197,28 @@ module Sprockets
 
         # Returns true when an asset will not be available after precompile is run
         def asset_needs_precompile?(source, filename)
-          if assets_environment && assets_environment.send(:matches_filter, precompile || [], source, filename)
+          if assets_environment && asset_matches_filter(precompile || [], source, filename)
             false
           else
             true
+          end
+        end
+
+        def asset_matches_filter(filters, logical_path, filename)
+          return true if filters.empty?
+
+          filters.any? do |filter|
+            if filter.is_a?(Regexp)
+              filter.match(logical_path)
+            elsif filter.respond_to?(:call)
+              if filter.arity == 1
+                filter.call(logical_path)
+              else
+                filter.call(logical_path, filename.to_s)
+              end
+            else
+              File.fnmatch(filter.to_s, logical_path)
+            end
           end
         end
 
