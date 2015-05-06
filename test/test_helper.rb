689eebe813be4f3de8fa3a3118d0635f63f74cdd
Joshua Peek <josh@joshpeek.com>
2015-05-06 15:13:39 -0700

    Test only apply to 3.x

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -700,25 +700,27 @@ class AutomaticDependenciesFromHelpersTest < HelperTest
     assert_includes paths, "missing.css.erb"
   end
 
-  def test_asset_path_with_index_requires_exact_logical_path
-    Sprockets::Rails::Helper.raise_runtime_errors = true
-    Sprockets::Rails::Helper.precompile = ["bundle.js"]
+  if Sprockets::VERSION > "3"
+    def test_asset_path_with_index_requires_exact_logical_path
+      Sprockets::Rails::Helper.raise_runtime_errors = true
+      Sprockets::Rails::Helper.precompile = ["bundle.js"]
 
-    assert @view.asset_path("bundle.js")
+      assert @view.asset_path("bundle.js")
 
-    assert_raises(Sprockets::Rails::Helper::AssetAliasUsed) do
-      assert @view.asset_path("bundle/index.js")
+      assert_raises(Sprockets::Rails::Helper::AssetAliasUsed) do
+        assert @view.asset_path("bundle/index.js")
+      end
     end
-  end
 
-  def test_asset_path_with_bower_requires_exact_logical_path
-    Sprockets::Rails::Helper.raise_runtime_errors = true
-    Sprockets::Rails::Helper.precompile = ["jquery/jquery.js"]
+    def test_asset_path_with_bower_requires_exact_logical_path
+      Sprockets::Rails::Helper.raise_runtime_errors = true
+      Sprockets::Rails::Helper.precompile = ["jquery/jquery.js"]
 
-    assert @view.asset_path("jquery/jquery.js")
+      assert @view.asset_path("jquery/jquery.js")
 
-    assert_raises(Sprockets::Rails::Helper::AssetAliasUsed) do
-      assert @view.asset_path("jquery.js")
+      assert_raises(Sprockets::Rails::Helper::AssetAliasUsed) do
+        assert @view.asset_path("jquery.js")
+      end
     end
   end
 end
