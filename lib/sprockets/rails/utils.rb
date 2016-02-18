6cc31598eaa82c8c937cb3dc9d437589dc920bee
Richard Schneeman <richard.schneeman@gmail.com>
2016-02-18 16:16:50 -0600

    Add support for beta version of sprockets 4

--- lib/sprockets/rails/utils.rb
+++ lib/sprockets/rails/utils.rb
@@ -4,7 +4,7 @@ module Sprockets
   module Rails
     module Utils
       def using_sprockets4?
-        Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new('4.0.0')
+        Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new('4.x')
       end
     end
   end
