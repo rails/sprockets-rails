408aae2f7183503cc3048b067d49deaa8719b6bb
David Heinemeier Hansson <david@basecamp.com>
2021-10-18 20:38:33 +0200

    Style

--- lib/sprockets/rails/asset_url_processor.rb
+++ lib/sprockets/rails/asset_url_processor.rb
@@ -6,9 +6,8 @@ module Sprockets
       
       def self.call(input)
         context = input[:environment].context_class.new(input)
-        data = input[:data].gsub(REGEX) do |_match|
-          "url(#{context.asset_path($1)})"
-        end
+        data    = input[:data].gsub(REGEX, "url(#{context.asset_path($1)})")
+
         { data: data }
       end
     end
