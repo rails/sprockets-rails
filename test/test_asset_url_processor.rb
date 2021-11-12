d667d47afd9de6363402bb74850c073c9bae58a0
thomas morgan <tm@iprog.com>
2021-11-12 10:56:39 -0700

    expose dependencies from AssetUrlProcessor

--- test/test_asset_url_processor.rb
+++ test/test_asset_url_processor.rb
@@ -4,36 +4,48 @@ require 'sprockets/railtie'
 
 Minitest::Test = MiniTest::Unit::TestCase unless defined?(Minitest::Test)
 class TestAssetUrlProcessor < Minitest::Test
+  FIXTURES_PATH = File.expand_path("../fixtures", __FILE__)
+
   def setup
     @env = Sprockets::Environment.new
+    @env.append_path FIXTURES_PATH
     @env.context_class.class_eval do
-      def asset_path(path, options = {})
-        'image-hexcodegoeshere.png'
-      end
+      include ::Sprockets::Rails::Context
     end
+    @env.context_class.digest_assets = true
+
+    @logo_digest = @env["logo.png"].etag
+    @logo_uri    = @env["logo.png"].uri
   end
 
   def test_basic
-    input = { environment: @env, data: 'background: url(image.png);', filename: 'url2.css', metadata: {} }
+    input = { environment: @env, data: 'background: url(logo.png);', filename: 'url2.css', metadata: {} }
     output = Sprockets::Rails::AssetUrlProcessor.call(input)
-    assert_equal({ data: "background: url(image-hexcodegoeshere.png);" }, output)
+    assert_equal("background: url(/logo-#{@logo_digest}.png);", output[:data])
   end
 
   def test_spaces
-    input = { environment: @env, data: 'background: url( image.png );', filename: 'url2.css', metadata: {} }
+    input = { environment: @env, data: 'background: url( logo.png );', filename: 'url2.css', metadata: {} }
     output = Sprockets::Rails::AssetUrlProcessor.call(input)
-    assert_equal({ data: "background: url(image-hexcodegoeshere.png);" }, output)
+    assert_equal("background: url(/logo-#{@logo_digest}.png);", output[:data])
   end
 
   def test_single_quote
-    input = { environment: @env, data: "background: url('image.png');", filename: 'url2.css', metadata: {} }
+    input = { environment: @env, data: "background: url('logo.png');", filename: 'url2.css', metadata: {} }
     output = Sprockets::Rails::AssetUrlProcessor.call(input)
-    assert_equal({ data: "background: url(image-hexcodegoeshere.png);" }, output)
+    assert_equal("background: url(/logo-#{@logo_digest}.png);", output[:data])
   end
 
   def test_double_quote
-    input = { environment: @env, data: 'background: url("image.png");', filename: 'url2.css', metadata: {} }
+    input = { environment: @env, data: 'background: url("logo.png");', filename: 'url2.css', metadata: {} }
     output = Sprockets::Rails::AssetUrlProcessor.call(input)
-    assert_equal({ data: "background: url(image-hexcodegoeshere.png);" }, output)
+    assert_equal("background: url(/logo-#{@logo_digest}.png);", output[:data])
+  end
+
+  def test_dependencies_are_tracked
+    input = { environment: @env, data: 'background: url(logo.png);', filename: 'url2.css', metadata: {} }
+    output = Sprockets::Rails::AssetUrlProcessor.call(input)
+    assert_equal(1, output[:links].size)
+    assert_equal(@logo_uri, output[:links].first)
   end
 end
