4571493dad9db368bfa70664d7c1cad33a138302
David Heinemeier Hansson <david@hey.com>
2021-11-15 15:55:53 +0100

    Warn in the log about removing the sourceMappingURL on missing assets

--- lib/sprockets/rails/sourcemapping_url_processor.rb
+++ lib/sprockets/rails/sourcemapping_url_processor.rb
@@ -5,12 +5,14 @@ module Sprockets
       REGEX = /\/\/# sourceMappingURL=(.*\.map)/
 
       def self.call(input)
-        context = input[:environment].context_class.new(input)
+        env     = input[:environment]
+        context = env.context_class.new(input)
         data    = input[:data].gsub(REGEX) do |_match|
           ensure_file_is_present = context.resolve($1)
           "//# sourceMappingURL=#{context.asset_path($1)}\n//!\n"
         rescue Sprockets::FileNotFound
-          # Remove source mapping when the target cannot be found
+          env.logger.warn "Removed sourceMappingURL comment for missing asset '#{$1}' from #{input[:filename]}"
+          nil
         end
 
         { data: data }
