dcdf365c69a2887beac63f04b511498efae3b1e2
Rafael Mendonça França <rafael@rubyonrails.org>
2021-11-19 19:20:41 +0000

    Whitespaces :scissors:

--- lib/sprockets/rails/asset_url_processor.rb
+++ lib/sprockets/rails/asset_url_processor.rb
@@ -3,7 +3,7 @@ module Sprockets
     # Rewrites urls in CSS files with the digested paths
     class AssetUrlProcessor
       REGEX = /url\(\s*["']?(?!(?:\#|data|http))([^"'\s)]+)\s*["']?\)/
-      
+
       def self.call(input)
         context = input[:environment].context_class.new(input)
         data    = input[:data].gsub(REGEX) { |_match| "url(#{context.asset_path($1)})" }
