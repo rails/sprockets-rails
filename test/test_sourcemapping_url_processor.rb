0a56eec89d86d7ef36af439bad7df51adbc575c5
David Heinemeier Hansson <david@basecamp.com>
2021-11-22 20:26:39 +0100

    Fix sourcemapping url replacement (#484)
    * Fix sourcemapping url replacement
    
    * Improve naming
    
    * Add test for dealing with the deep nest issue

--- test/test_sourcemapping_url_processor.rb
+++ test/test_sourcemapping_url_processor.rb
@@ -1,7 +1,6 @@
 require 'minitest/autorun'
 require 'sprockets/railtie'
 
-
 Minitest::Test = MiniTest::Unit::TestCase unless defined?(Minitest::Test)
 class TestSourceMappingUrlProcessor < Minitest::Test
   def setup
@@ -11,17 +10,29 @@ class TestSourceMappingUrlProcessor < Minitest::Test
   def test_successful
     @env.context_class.class_eval do
       def resolve(path, **kargs)
-        "/yes"
+        "/assets/mapped.js.map"
       end
 
       def asset_path(path, options = {})
-        'mapped-HEXGOESHERE.js.map'
+        "/assets/mapped-HEXGOESHERE.js.map"
       end
     end
 
-    input = { environment: @env, data: "var mapped;\n//# sourceMappingURL=mapped.js.map", filename: 'mapped.js', metadata: {} }
+    input = { environment: @env, data: "var mapped;\n//# sourceMappingURL=mapped.js.map", name: 'mapped', filename: 'mapped.js', metadata: {} }
     output = Sprockets::Rails::SourcemappingUrlProcessor.call(input)
-    assert_equal({ data: "var mapped;\n//# sourceMappingURL=mapped-HEXGOESHERE.js.map\n//!\n" }, output)
+    assert_equal({ data: "var mapped;\n//# sourceMappingURL=/assets/mapped-HEXGOESHERE.js.map\n//!\n" }, output)
+  end
+
+  def test_resolving_erroneously_without_map_extension
+    @env.context_class.class_eval do
+      def resolve(path, **kargs)
+        "/assets/mapped.js"
+      end
+    end
+
+    input = { environment: @env, data: "var mapped;\n//# sourceMappingURL=mapped.js.map", name: 'mapped', filename: 'mapped.js', metadata: {} }
+    output = Sprockets::Rails::SourcemappingUrlProcessor.call(input)
+    assert_equal({ data: "var mapped;\n" }, output)
   end
 
   def test_missing
@@ -31,7 +42,7 @@ class TestSourceMappingUrlProcessor < Minitest::Test
       end
     end
 
-    input = { environment: @env, data: "var mapped;\n//# sourceMappingURL=mappedNOT.js.map", filename: 'mapped.js', metadata: {} }
+    input = { environment: @env, data: "var mapped;\n//# sourceMappingURL=mappedNOT.js.map", name: 'mapped', filename: 'mapped.js', metadata: {} }
     output = Sprockets::Rails::SourcemappingUrlProcessor.call(input)
     assert_equal({ data: "var mapped;\n" }, output)
   end
