05dbab27d54f58a7a505498863dd0d88a6b32389
Justin Coyne <jcoyne@justincoyne.com>
2021-11-22 09:56:58 -0600

    Strip away the relative path (#482)
    Fixes #478

--- lib/sprockets/rails/asset_url_processor.rb
+++ lib/sprockets/rails/asset_url_processor.rb
@@ -2,11 +2,13 @@ module Sprockets
   module Rails
     # Rewrites urls in CSS files with the digested paths
     class AssetUrlProcessor
-      REGEX = /url\(\s*["']?(?!(?:\#|data|http))([^"'\s)]+)\s*["']?\)/
-
+      REGEX = /url\(\s*["']?(?!(?:\#|data|http))(?<relativeToCurrentDir>.\/)?(?<path>[^"'\s)]+)\s*["']?\)/
       def self.call(input)
         context = input[:environment].context_class.new(input)
-        data    = input[:data].gsub(REGEX) { |_match| "url(#{context.asset_path($1)})" }
+        data    = input[:data].gsub(REGEX) do |_match|
+          path = Regexp.last_match[:path]
+          "url(#{context.asset_path(path)})"
+        end
 
         context.metadata.merge(data: data)
       end
