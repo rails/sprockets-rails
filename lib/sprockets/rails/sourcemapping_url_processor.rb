78b0f58e7b802b774a7b154d4e08a15d51f21d6d
David Heinemeier Hansson <david@hey.com>
2021-11-22 19:38:50 +0100

    Fix sourcemapping url replacement

--- lib/sprockets/rails/sourcemapping_url_processor.rb
+++ lib/sprockets/rails/sourcemapping_url_processor.rb
@@ -4,18 +4,50 @@ module Sprockets
     class SourcemappingUrlProcessor
       REGEX = /\/\/# sourceMappingURL=(.*\.map)/
 
-      def self.call(input)
-        env     = input[:environment]
-        context = env.context_class.new(input)
-        data    = input[:data].gsub(REGEX) do |_match|
-          context.resolve($1) # Ensure file is present
-          "//# sourceMappingURL=#{context.asset_path($1)}\n//!\n"
-        rescue Sprockets::FileNotFound
-          env.logger.warn "Removed sourceMappingURL comment for missing asset '#{$1}' from #{input[:filename]}"
-          nil
+      class << self
+        def call(input)
+          env     = input[:environment]
+          context = env.context_class.new(input)
+          data    = input[:data].gsub(REGEX) do |_match|
+            sourcemap_logical_path = combine_sourcemap_logical_path(sourcefile: input[:name], sourcemap: $1)
+
+            begin
+              sourcemap_comment(sourcemap_logical_path, context)
+            rescue Sprockets::FileNotFound
+              sourcemap_comment_removed(sourcemap_logical_path, filename: input[:filename], env: env)
+            end
+          end
+
+          { data: data }
         end
 
-        { data: data }
+        private
+          def combine_sourcemap_logical_path(sourcefile:, sourcemap:)
+            if (parts = sourcefile.split("/")).many?
+              parts[0..-2].append(sourcemap).join("/")
+            else
+              sourcemap
+            end
+          end
+
+          def sourcemap_comment(sourcemap_logical_path, context)
+            "//# sourceMappingURL=#{sourcemap_asset_path(sourcemap_logical_path, context: context)}\n//!\n"
+          end
+
+          def sourcemap_asset_path(sourcemap_logical_path, context:)
+            # FIXME: Work-around for bug where if the sourcemap is nested two levels deep, it'll resolve as the source file
+            # that's being mapped, rather than the map itself. So context.resolve("a/b/c.js.map") will return "c.js?"
+            if context.resolve(sourcemap_logical_path) =~ /\.map/
+              context.asset_path(sourcemap_logical_path)
+            else
+              raise Sprockets::FileNotFound, "Failed to resolve source map asset due to nesting depth"
+            end
+          end
+
+          def sourcemap_comment_removed(sourcemap_logical_path, filename:, env:)
+            env.logger.warn "Removed sourceMappingURL comment for missing asset '#{sourcemap_logical_path}' from #{filename}"
+            nil
+          end
       end
     end
   end
