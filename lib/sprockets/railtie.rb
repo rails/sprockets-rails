15dfcb6472028eb85fa8f7bd99a602f7e527668b
Hartley McGuire <skipkayhil@gmail.com>
2022-07-12 20:49:14 -0400

    Remove check for append_assets_path initializer
    It was removed from Rails in rails/rails@5172d93, and sprockets-rails
    only supports Rails 5.2+
    
    The initializer is also changed to be a simpler class method call
    instead of reopening Rails::Engine, similarly to how it was changed in
    rails/propshaft@c7d5542

--- lib/sprockets/railtie.rb
+++ lib/sprockets/railtie.rb
@@ -53,17 +53,6 @@ module Rails
       @precompiled_assets ||= assets_manifest.find(config.assets.precompile).map(&:logical_path).to_set
     end
   end
-
-  class Engine < Railtie
-    # Skip defining append_assets_path on Rails <= 4.2
-    unless initializers.find { |init| init.name == :append_assets_path }
-      initializer :append_assets_path, :group => :all do |app|
-        app.config.assets.paths.unshift(*paths["vendor/assets"].existent_directories)
-        app.config.assets.paths.unshift(*paths["lib/assets"].existent_directories)
-        app.config.assets.paths.unshift(*paths["app/assets"].existent_directories)
-      end
-    end
-  end
 end
 
 module Sprockets
@@ -94,6 +83,12 @@ module Sprockets
       def configure(&block)
         self._blocks << block
       end
+    end
+
+    ::Rails::Engine.initializer :append_assets_path, :group => :all do |app|
+      app.config.assets.paths.unshift(*paths["vendor/assets"].existent_directories)
+      app.config.assets.paths.unshift(*paths["lib/assets"].existent_directories)
+      app.config.assets.paths.unshift(*paths["app/assets"].existent_directories)
     end
 
     config.assets = OrderedOptions.new
